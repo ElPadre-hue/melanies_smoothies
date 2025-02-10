404ea2f11bf52999d6b03cba197dcd923eb42762
ElPadre-hue <vallejocarolina1@gmail.com>
2025-02-10 17:13:15 +0100

    Update streamli_app.py

--- streamli_app.py
+++ streamli_app.py
@@ -14,8 +14,13 @@ st.write('The name on your Smoothie will be:', name_on_order )
 
 cnx=st.connection('Snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC funtion
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list =st.multiselect(
     'Choose up to 5 ingredients:'
